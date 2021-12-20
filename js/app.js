92139d75f1ecaf73e8fcae0a4279bf9dae6c9b1b
paolaPerez00 <76698997+paolaPerez00@users.noreply.github.com>
2021-12-20 13:50:11 -0500

    Cambios factura

--- js/app.js
+++ js/app.js
@@ -177,31 +177,3 @@ async function sendCreate(url, data, meth) {
     })
     return response.json()
 }
-
-
-/*document.addEventListener('DOMContentLoaded', e => {
-    fetchData()
-    if (localStorage.getItem('carrito')) {
-        carrito = JSON.parse(localStorage.getItem('carrito'))
-        pintarCarrito()
-    }
-});*/
-
-/*const pintarCarrito = () => {
-    items.innerHTML = ''
-    Object.values(carrito).forEach(producto => {
-        templateCarrito.querySelector('th').textContent = producto.id
-        templateCarrito.querySelectorAll('td')[0].textContent = producto.title
-        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
-        templateCarrito.querySelector('span').textContent = producto.precio * producto.cantidad
-        
-        //botones
-        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
-        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
-        const clone = templateCarrito.cloneNode(true)
-        fragment.appendChild(clone)
-    })
-    items.appendChild(fragment)
-    pintarFooter()
-    localStorage.setItem('carrito', JSON.stringify(carrito))
-}*/
